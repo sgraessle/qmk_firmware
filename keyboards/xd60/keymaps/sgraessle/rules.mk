74c2089a83b1e47abb9f40f23541935d90cbcbad
Scott Graessle <sgraessle@zynga.com>
2018-11-30 16:56:04 -0600

    Enable MacOS media keys.

---
+++ keyboards/xd60/keymaps/sgraessle/rules.mk
@@ -0,0 +1,5 @@
+# Build Options
+BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes      # Audio control and System control(+450)
+NKRO_ENABLE = no           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
